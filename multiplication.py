e41586d68e2b3269f26fe23aef5ad403a3de118f
BartolomeoML <kustklevera@gmail.com>
2026-06-02 23:22:53 +0300

    Change main script

--- multiplication.py
+++ multiplication.py
@@ -1,1 +1,9 @@
-print("Hello, World!")
+# Запрашиваем у пользователя целое число
+number = int(input("Введите целое число: "))
+
+# Выводим таблицу умножения
+print(f"\nТаблица умножения для числа {number}:\n")
+
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} × {i} = {result}")
